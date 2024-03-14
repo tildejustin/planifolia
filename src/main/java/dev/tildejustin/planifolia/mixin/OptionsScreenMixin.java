b51b4af26ae1243fc4ea1db98c7be475f1f1caa0
tildejustin <spacepepper3.14@gmail.com>
2024-03-14 17:19:01 -0400

    modifyarg vanilla options screen

--- src/main/java/dev/tildejustin/planifolia/mixin/OptionsScreenMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/OptionsScreenMixin.java
@@ -1,13 +1,12 @@
 package dev.tildejustin.planifolia.mixin;
 
 import com.bawnorton.mixinsquared.TargetHandler;
-import com.llamalad7.mixinextras.injector.wrapoperation.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.*;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(value = OptionsScreen.class, priority = 1005)
 public abstract class OptionsScreenMixin extends Screen {
@@ -17,12 +16,8 @@ public abstract class OptionsScreenMixin extends Screen {
 
     @Dynamic
     @TargetHandler(mixin = "me.jellysquid.mods.sodium.mixin.features.options.MixinOptionsScreen", name = "open")
-    @WrapOperation(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
-    private void openVanillaMenu(MinecraftClient instance, Screen screen, Operation<Void> original) {
-        if (MinecraftClient.getInstance().world != null) {
-            this.client.setScreen(new VideoOptionsScreen(this, this.client.options));
-        } else {
-            original.call(instance, screen);
-        }
+    @ModifyArg(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
+    private Screen openVanillaMenu(Screen original) {
+        return MinecraftClient.getInstance().world != null ? new VideoOptionsScreen(this, this.client.options) : original;
     }
 }
