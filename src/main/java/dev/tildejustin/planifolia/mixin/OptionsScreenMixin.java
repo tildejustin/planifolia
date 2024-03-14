374dbd80a74b14c36b0ed890e975d7c2700a3818
tildejustin <spacepepper3.14@gmail.com>
2024-03-14 17:19:01 -0400

    modifyarg vanilla options screen

--- src/main/java/dev/tildejustin/planifolia/mixin/OptionsScreenMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/OptionsScreenMixin.java
@@ -1,14 +1,12 @@
 package dev.tildejustin.planifolia.mixin;
 
 import com.bawnorton.mixinsquared.TargetHandler;
-import com.llamalad7.mixinextras.injector.wrapoperation.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.*;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = OptionsScreen.class, priority = 1005)
 public abstract class OptionsScreenMixin extends Screen {
@@ -19,25 +17,16 @@ public abstract class OptionsScreenMixin extends Screen {
     @Dynamic
     @Group(min = 1, max = 1)
     @TargetHandler(mixin = "me.jellysquid.mods.sodium.mixin.features.options.MixinOptionsScreen", name = "open")
-    @WrapOperation(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;setReturnValue(Ljava/lang/Object;)V"), require = 0)
-    private void openVanillaMenu1(CallbackInfoReturnable<Screen> instance, Object screen, Operation<Void> original) {
-        openVanillaMenu(instance, screen, original);
+    @ModifyArg(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;setReturnValue(Ljava/lang/Object;)V"), require = 0)
+    private Object openVanillaMenu1(Object original) {
+        return MinecraftClient.getInstance().world != null ? new VideoOptionsScreen(this, this.client.options) : original;
     }
 
     @Dynamic
     @Group
     @TargetHandler(mixin = "me.jellysquid.mods.sodium.mixin.features.gui.hooks.settings.OptionsScreenMixin", name = "open")
-    @WrapOperation(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;setReturnValue(Ljava/lang/Object;)V"), require = 0)
-    private void openVanillaMenu2(CallbackInfoReturnable<Screen> instance, Object screen, Operation<Void> original) {
-        openVanillaMenu(instance, screen, original);
-    }
-
-    @Unique
-    private void openVanillaMenu(CallbackInfoReturnable<Screen> instance, Object screen, Operation<Void> original) {
-        if (MinecraftClient.getInstance().world != null) {
-            instance.setReturnValue(new VideoOptionsScreen(this, this.client.options));
-        } else {
-            original.call(instance, screen);
-        }
+    @ModifyArg(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;setReturnValue(Ljava/lang/Object;)V"), require = 0)
+    private Object openVanillaMenu2(Object original) {
+        return MinecraftClient.getInstance().world != null ? new VideoOptionsScreen(this, this.client.options) : original;
     }
 }
