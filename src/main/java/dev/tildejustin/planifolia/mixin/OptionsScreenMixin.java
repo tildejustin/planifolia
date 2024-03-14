60a4f4e2ae6903e7e5cf73f7cfae5ec01fed411b
tildejustin <spacepepper3.14@gmail.com>
2024-03-14 17:05:43 -0400

    fix latest 1.20 sodium

--- src/main/java/dev/tildejustin/planifolia/mixin/OptionsScreenMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/OptionsScreenMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.*;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = OptionsScreen.class, priority = 1005)
@@ -17,8 +17,22 @@ public abstract class OptionsScreenMixin extends Screen {
     }
 
     @Dynamic
+    @Group(min = 1, max = 1)
     @TargetHandler(mixin = "me.jellysquid.mods.sodium.mixin.features.options.MixinOptionsScreen", name = "open")
-    @WrapOperation(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;setReturnValue(Ljava/lang/Object;)V"))
+    @WrapOperation(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;setReturnValue(Ljava/lang/Object;)V"), require = 0)
+    private void openVanillaMenu1(CallbackInfoReturnable<Screen> instance, Object screen, Operation<Void> original) {
+        openVanillaMenu(instance, screen, original);
+    }
+
+    @Dynamic
+    @Group
+    @TargetHandler(mixin = "me.jellysquid.mods.sodium.mixin.features.gui.hooks.settings.OptionsScreenMixin", name = "open")
+    @WrapOperation(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lorg/spongepowered/asm/mixin/injection/callback/CallbackInfoReturnable;setReturnValue(Ljava/lang/Object;)V"), require = 0)
+    private void openVanillaMenu2(CallbackInfoReturnable<Screen> instance, Object screen, Operation<Void> original) {
+        openVanillaMenu(instance, screen, original);
+    }
+
+    @Unique
     private void openVanillaMenu(CallbackInfoReturnable<Screen> instance, Object screen, Operation<Void> original) {
         if (MinecraftClient.getInstance().world != null) {
             instance.setReturnValue(new VideoOptionsScreen(this, this.client.options));
