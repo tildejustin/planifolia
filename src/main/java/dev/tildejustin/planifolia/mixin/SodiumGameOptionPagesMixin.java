50068783b8e026845cd2d930d19b8826fd1d6b28
tildejustin <spacepepper3.14@gmail.com>
2024-03-15 21:01:59 -0400

    validate loaded gamma instead of displaying a custom text, remove certain sodium options and force them to follow global graphics settings

--- src/main/java/dev/tildejustin/planifolia/mixin/SodiumGameOptionPagesMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/SodiumGameOptionPagesMixin.java
@@ -1,6 +1,7 @@
 package dev.tildejustin.planifolia.mixin;
 
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
+import me.jellysquid.mods.sodium.client.gui.options.*;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
@@ -16,5 +17,34 @@ public abstract class SodiumGameOptionPagesMixin {
     )
     private static int modifyGammaSliderMaximum(int original) {
         return MinecraftClient.getInstance().world == null ? 500 : original;
+    }
+
+    @Redirect(
+            method = "quality",
+            at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;add(Lme/jellysquid/mods/sodium/client/gui/options/Option;)Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;", ordinal = 0),
+            slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=soundCategory.weather")),
+            remap = false
+    )
+    private static OptionGroup.Builder removeWeatherDisablingOption(OptionGroup.Builder instance, Option<?> option) {
+        return instance;
+    }
+
+    @Redirect(
+            method = "quality",
+            at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;add(Lme/jellysquid/mods/sodium/client/gui/options/Option;)Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;", ordinal = 0),
+            slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=sodium.options.leaves_quality.name")),
+            remap = false
+    )
+    private static OptionGroup.Builder removeLeaveQualityOption(OptionGroup.Builder instance, Option<?> option) {
+        return instance;
+    }
+
+    @Redirect(
+            method = "quality",
+            at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;add(Lme/jellysquid/mods/sodium/client/gui/options/Option;)Lme/jellysquid/mods/sodium/client/gui/options/OptionGroup$Builder;", ordinal = 0),
+            slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=sodium.options.vignette.name")), remap = false
+    )
+    private static OptionGroup.Builder removeVignetteOption(OptionGroup.Builder instance, Option<?> option) {
+        return instance;
     }
 }
