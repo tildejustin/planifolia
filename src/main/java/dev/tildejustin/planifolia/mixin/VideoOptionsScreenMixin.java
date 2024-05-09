868f179dfa66e4c99b4b0961b2b76aa6ca0dc965
tildejustin <spacepepper3.14@gmail.com>
2024-05-08 20:54:20 -0400

    recreate Entity Culling SimpleOption every time a VideoOptionsScreen is created, fixing a visual desync when StandardSettings updates the variable directly

--- src/main/java/dev/tildejustin/planifolia/mixin/VideoOptionsScreenMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/VideoOptionsScreenMixin.java
@@ -3,7 +3,7 @@ package dev.tildejustin.planifolia.mixin;
 import net.caffeinemc.mods.sodium.client.SodiumClientMod;
 import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
 import net.minecraft.client.option.*;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -11,17 +11,14 @@ import java.util.Arrays;
 
 @Mixin(VideoOptionsScreen.class)
 public abstract class VideoOptionsScreenMixin {
-    @Unique
-    private static final SimpleOption<Boolean> ENTITY_CULLING = SimpleOption.ofBoolean(
-            "Entity Culling",
-            SodiumClientMod.options().performance.useEntityCulling,
-            entityCulling -> SodiumClientMod.options().performance.useEntityCulling = entityCulling
-    );
-
     @Inject(method = "getOptions", at = @At("TAIL"), cancellable = true)
     private static void addEntityCullingOption(GameOptions gameOptions, CallbackInfoReturnable<SimpleOption<?>[]> cir) {
         SimpleOption<?>[] newOptions = Arrays.copyOf(cir.getReturnValue(), cir.getReturnValue().length + 1);
-        newOptions[newOptions.length - 1] = ENTITY_CULLING;
+        newOptions[newOptions.length - 1] = SimpleOption.ofBoolean(
+                "Entity Culling",
+                SodiumClientMod.options().performance.useEntityCulling,
+                entityCulling -> SodiumClientMod.options().performance.useEntityCulling = entityCulling
+        );
         cir.setReturnValue(newOptions);
     }
 }
