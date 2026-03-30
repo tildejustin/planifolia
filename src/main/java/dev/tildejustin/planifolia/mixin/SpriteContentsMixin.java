bb2666922e0c3c3f0230720bc37af6e61bdb6bdf
tildejustin <tildejustin@gmail.com>
2026-03-30 10:29:28 -0400

    rewrite sprite mixin

--- src/main/java/dev/tildejustin/planifolia/mixin/SpriteContentsMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/SpriteContentsMixin.java
@@ -1,43 +1,23 @@
 package dev.tildejustin.planifolia.mixin;
 
 import com.bawnorton.mixinsquared.TargetHandler;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import net.minecraft.client.texture.NativeImage;
-import net.minecraft.client.texture.SpriteContents;
-import net.minecraft.client.texture.SpriteDimensions;
-import net.minecraft.resource.metadata.ResourceMetadata;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.client.texture.*;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = SpriteContents.class, priority = 1500)
 public class SpriteContentsMixin {
-    @Mutable
     @Shadow
     @Final
-    private NativeImage image;
-
-    /**
-     * We can't use a MixinExtras @Share here because we're not targeting the same method,
-     * therefore we need to store it the good old way™
-     */
-    @Unique
-    private static Identifier idRef = null;
-
-    @Inject(method = "<init>", at = @At("HEAD"))
-    private static void storeIdentifier(Identifier id, SpriteDimensions dimensions, NativeImage image, ResourceMetadata metadata, CallbackInfo ci) {
-        idRef = id;
-    }
+    private Identifier id;
 
     @Dynamic
-    @TargetHandler(mixin = "net.caffeinemc.mods.sodium.mixin.features.textures.scan.SpriteContentsMixin", name = "sodium$beforeGenerateMipLevels", prefix = "wrapOperation")
-    @Inject(method = "@MixinSquared:Handler", at = @At("HEAD"), cancellable = true)
-    public void skipSodiumScan(SpriteContents instance, NativeImage nativeImage, Operation<Void> original, CallbackInfo ci) {
-        if (idRef != null && idRef.getPath().contains("leaves")) {
-            this.image = nativeImage;
-            ci.cancel();
-        }
+    @TargetHandler(mixin = "net.caffeinemc.mods.sodium.mixin.features.textures.mipmaps.SpriteContentsMixin", name = "sodium$beforeGenerateMipLevels", prefix = "wrapOperation")
+    // sodium$fillInTransparentPixelColors is an @Unique merged method
+    @WrapWithCondition(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_7764;sodium$fillInTransparentPixelColors(Lnet/minecraft/class_1011;)V"))
+    public boolean skipSodiumFill(NativeImage nativeImage) {
+        return !id.getPath().contains("leaves");
     }
 }
