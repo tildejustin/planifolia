effdb48ab556afb95eaa31103a93076c7b76af67
tildejustin <tildejustin@gmail.com>
2026-05-24 04:32:20 -0400

    1.2.4: 26.1.2 sodium support & work with standardsettings

--- src/main/java/dev/tildejustin/planifolia/mixin/MinecraftClientMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/MinecraftClientMixin.java
@@ -8,13 +8,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+@Mixin(value = Minecraft.class, priority = 900)
 public abstract class MinecraftClientMixin {
-    @Inject(method = "run", at = @At("HEAD"))
-    private void removeSodiumDebugHudEntry(CallbackInfo ci) {
+    // need to run after client modinit (top of <init>) and before srapi postlaunch (bottom of <init>)
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private static void removeSodiumDebugHudEntry(CallbackInfo ci) {
         // Note for porting, sodium does a gross accessor to add its debug hud entry, this removes it, in future
         // versions they may use something from Fabric API, so adjust accordingly
         DebugScreenEntriesAccessor.sodium$getEntries().remove(Identifier.fromNamespaceAndPath("sodium", "debug_full"));
         DebugScreenEntriesAccessor.sodium$getEntries().remove(Identifier.fromNamespaceAndPath("sodium", "debug_reduced"));
+        DebugScreenEntriesAccessor.sodium$getEntries().remove(Identifier.fromNamespaceAndPath("sodium", "fps_percentiles"));
     }
 }
