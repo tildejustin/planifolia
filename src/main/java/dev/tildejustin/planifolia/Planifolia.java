ca74fb61e5762c6e42c21aea4d2cfa65abf48ff9
tildejustin <tildejustin@gmail.com>
2026-09-19 18:00:15 -0400

    restrict gamma 5 slider to only at launch

---
+++ src/main/java/dev/tildejustin/planifolia/Planifolia.java
@@ -0,0 +1,9 @@
+package dev.tildejustin.planifolia;
+
+public class Planifolia {
+    public static boolean setLevel = false;
+
+    public static boolean restrictGamma() {
+        return setLevel;
+    }
+}
