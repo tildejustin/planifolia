2d044d03b5bee5234dedc3a9162d919db01ca4a8
tildejustin <tildejustin@gmail.com>
2026-09-19 18:00:15 -0400

    restrict gamma 5 slider to only at launch

---
+++ src/main/java/dev/tildejustin/planifolia/mixin/MinecraftClientMixin.java
@@ -0,0 +1,17 @@
+package dev.tildejustin.planifolia.mixin;
+
+import dev.tildejustin.planifolia.Planifolia;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.DownloadingTerrainScreen;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+    @Inject(method = "joinWorld", at = @At("HEAD"))
+    private void storeSetLevel(ClientWorld world, DownloadingTerrainScreen.WorldEntryReason worldEntryReason, CallbackInfo ci) {
+        if (world != null) Planifolia.setLevel = true;
+    }
+}
