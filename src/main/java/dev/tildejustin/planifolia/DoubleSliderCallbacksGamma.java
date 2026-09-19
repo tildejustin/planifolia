fb0aea475ae4cc6e74d3bb46934cda48d5dd393c
tildejustin <tildejustin@gmail.com>
2026-09-19 18:00:15 -0400

    restrict gamma 5 slider to only at launch

--- src/main/java/dev/tildejustin/planifolia/DoubleSliderCallbacksGamma.java
+++ src/main/java/dev/tildejustin/planifolia/DoubleSliderCallbacksGamma.java
@@ -2,7 +2,6 @@ package dev.tildejustin.planifolia;
 
 import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.SimpleOption;
 
 import java.util.Optional;
@@ -17,15 +16,12 @@ public enum DoubleSliderCallbacksGamma implements SimpleOption.SliderCallbacks<D
 
     @Override
     public double toSliderProgress(Double value) {
-        // when not in a world, value may be is 0 -> 5, and is scaled to 0 -> 1 on the slider
-        // when in a world, any value > 1 will be rendered at 1, via SliderWidgetMixin
-        return MinecraftClient.getInstance().world == null ? value / 5 : value;
+        return Planifolia.restrictGamma() ? value : value / 5;
     }
 
     @Override
     public Double toValue(double progress) {
-        // when not in a world, a slider progress is interpreted as being from 0 -> 5, when in a world it is the standard 0 -> 1
-        return MinecraftClient.getInstance().world == null ? progress * 5 : progress;
+        return Planifolia.restrictGamma() ? progress : progress * 5;
     }
 
     @Override
