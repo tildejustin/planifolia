ed2e5c677c8b4daefe2f24a970b588401bcab0ea
tildejustin <tildejustin@gmail.com>
2026-09-19 20:42:02 -0400

    remove draftout code

--- src/main/java/dev/tildejustin/planifolia/Planifolia.java
+++ src/main/java/dev/tildejustin/planifolia/Planifolia.java
@@ -1,12 +1,9 @@
 package dev.tildejustin.planifolia;
 
-import net.fabricmc.loader.api.FabricLoader;
-
 public class Planifolia {
     public static boolean setLevel = false;
-    private static final boolean draftoutPresent = FabricLoader.getInstance().isModLoaded("draftout");
 
     public static boolean restrictGamma() {
-        return setLevel && !draftoutPresent;
+        return setLevel;
     }
 }
